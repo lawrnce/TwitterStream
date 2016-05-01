afc9a95f23c95bae1e0e000bc0fc52cc780174db
Lawrence Tran <lawrence@tran.com>
2016-05-01 17:06:01 -0500

    Added stream pause

--- TwitterStream/TwitterManager.swift
+++ TwitterStream/TwitterManager.swift
@@ -22,38 +22,31 @@ class TwitterManager: NSObject {
     
     var delegate: TwitterManagerDelegate?
     
-    // retain the keyword for pause and resume
+    // Retrain current keyword to resume stream
     private var currentKeyword: String!
     
-    private var currentConnection: NSURLConnection!
-    
-    private var currentRequest: SLRequest!
+    // Boolean to check if user paused the stream
+    private var shouldPause: Bool!
     
     override init() {
         super.init()
         self.isConnected = false
         self.isTryingToConnect = false
+        self.shouldPause = false
     }
     
     /**
         NSURLConnection cannot be paused. So we cancel it.
      */
     func pauseStream() {
-        print("Paused connection")
-//        self.currentConnection.cancel()
-        self.isConnected = false
-        self.isTryingToConnect = false
-        
-        dispatch_async(dispatch_get_main_queue(), { () -> Void in
-            self.currentConnection.cancel()
-            self.currentConnection = nil
-        })
+        self.shouldPause = true
     }
     
     /**
         Create a new connection with the same keyword.
      */
     func resumeStream() {
+        self.shouldPause = false
         createStreamConnectionForKeyword(self.currentKeyword)
     }
     
@@ -104,8 +97,8 @@ class TwitterManager: NSObject {
                             request.account = account
                             
                             dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                                self.currentConnection = NSURLConnection(request: request.preparedURLRequest(), delegate: self)
-                                self.currentConnection?.start()
+                                let connection = NSURLConnection(request: request.preparedURLRequest(), delegate: self)
+                                connection?.start()
                                 self.currentKeyword = keyword
                             })
                             
@@ -137,6 +130,13 @@ extension TwitterManager: NSURLConnectionDataDelegate {
         When data is received, parse it to a JSON and notifiy delegate.
      */
     func connection(connection: NSURLConnection, didReceiveData data: NSData) {
+        
+        // Check if user paused stream
+        if (self.shouldPause == true) {
+            connection.cancel()
+            print("Connection Paused")
+            return
+        }
         
         // Cast response data to JSON
         let json = JSON(data: data)
